48d0843774fd11947d76a1b278e33a7b38743e01
Roman <romandgo09@gmail.com>
2022-05-24 13:46:08 +0300

    Create footer component

--- dist/main.ab135317e93c15c9b494.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";alert("yo")})();
-//# sourceMappingURL=main.ab135317e93c15c9b494.js.map
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,7 +13,6 @@ function closeAllSubMenu() {
 }
 
 document.querySelector('.header__burger').onclick = function(event) {
-   document.body.classList.toggle('_lock');
    event.target.classList.toggle('header__burger_active');
    menuBody.classList.toggle('header__menu_active');
 };
